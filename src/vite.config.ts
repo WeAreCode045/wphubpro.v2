74e697983d110b9abe59199e9ca67ba106deb42c
WeAreCode045 <maurice@code045.nl>
2026-02-24 03:10:23 +0100

    fix: update output directory in vite.config.ts and remove vercel.json

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true,
   },
 })
